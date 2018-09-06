db34c9f1c022bf0f421bfb2049c30ebe7ca3dc5d
Formacion <Formacion@R9E4XEZ.insags.com>
2018-09-06 22:01:33 +0200

    Ejemplo Clase Interna

---
+++ cajondesastre/src/com/rvssoft/cajondesastre/model/claseinterna/ClaseInterna.java
@@ -0,0 +1,24 @@
+package com.rvssoft.cajondesastre.model.claseinterna;
+
+/**
+ * 
+ * @author Formacion
+ *
+ */
+public class ClaseInterna {
+
+	public static void main(String[] args) {
+
+		Robot.DescripcionRobot r = (new Robot(3)).new DescripcionRobot();
+		
+		System.out.println(r);
+
+	 
+	
+		
+		 
+
+		
+	}
+
+}
