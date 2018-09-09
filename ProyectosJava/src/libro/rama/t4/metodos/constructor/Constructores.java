7070a4554b2aa5c6ba3ce8cd49c59a1cb6b03627
Formacion <Formacion@R9E4XEZ.insags.com>
2018-09-09 10:11:19 +0200

    Constructor y Referencias

--- ProyectosJava/src/libro/rama/t4/metodos/constructor/Constructores.java
+++ ProyectosJava/src/libro/rama/t4/metodos/constructor/Constructores.java
@@ -7,8 +7,26 @@
 
 package libro.rama.t4.metodos.constructor;
 
-
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Constructores {
 
+	private Date alta;
+
+	public Constructores() {
+
+		GregorianCalendar cal = new GregorianCalendar(1984, 10, 15);
+		System.out.println(alta = cal.getTime());
+	}
+
+	public Date getAlta() {
+		return alta;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(new Constructores());
+
+	}
 }
