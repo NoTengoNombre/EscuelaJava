31b79436dfe3be65554904a2d7eb249596678db2
Formacion <Formacion@R9E4XEZ.insags.com>
2018-09-09 15:49:44 +0200

    Wrapper basicos

---
+++ ProyectosJava/src/libro/rama/t4/wrapper/Integer.java
@@ -0,0 +1,29 @@
+/**
+ * @see 
+ * @since 9 sept. 2018 : 15:39:10
+ * @author Formacion
+ *
+*/
+
+package libro.rama.t4.wrapper;
+
+public class UsoInteger {
+
+	String y;
+
+	public Integer(int i) {
+		y = String.valueOf(i);
+	}
+
+	public static void main(String[] args) {
+
+		Integer i0 = new Integer(1);
+		System.out.println("Crear " + i0);
+		
+
+		int x = 10;
+		Integer i1 = new Integer(x);
+
+		
+	}
+}
